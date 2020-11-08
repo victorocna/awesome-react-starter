58eb05a159723c6816a7d85f7513cd24d65962dd
victorocna <victorocnarescu@gmail.com>
2020-11-08 19:53:11 +0200

    update quote loading

--- components/Demo/QuoteLoading.jsx
+++ components/Demo/QuoteLoading.jsx
@@ -4,11 +4,11 @@ const QuoteLoading = () => (
   <div className="animate-pulse">
     <div className="flex flex-col px-4 lg:px-8 bg-white py-2">
       <dt className="lg:w-1/4">Author: </dt>
-      <dd className="h-4 bg-gray-300 rounded-lg w-1/2"></dd>
+      <dd className="h-4 my-1 bg-gray-300 rounded-lg w-1/2"></dd>
     </div>
     <div className="flex flex-col px-4 lg:px-8 bg-white py-2">
       <dt className="lg:w-1/4">Quote: </dt>
-      <dd className="h-4 bg-gray-300 rounded-lg w-1/2"></dd>
+      <dd className="h-4 my-1 bg-gray-300 rounded-lg w-1/2"></dd>
     </div>
   </div>
 );

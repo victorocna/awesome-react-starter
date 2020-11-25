afbffa4fe4938a296ec202b1fc0643a865c8ad1d
victorocna <victorocnarescu@gmail.com>
2020-11-25 18:05:34 +0200

    log errors in check auth logic

--- services/auth/check-auth.js
+++ services/auth/check-auth.js
@@ -4,6 +4,9 @@ const checkAuth = async (context) => {
   try {
     await ensureUser(context.req.headers.cookie);
   } catch (err) {
+    console.warn('Could not ensure user is logged in or refresh their token');
+    console.error(err);
+
     return {
       redirect: {
         destination: '/login',

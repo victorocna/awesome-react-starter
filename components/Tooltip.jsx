867c3c5bdbb20d4615f7cf700c50a8f1f21f57db
Radu Toma <radutoma337@gmail.com>
2021-02-28 16:01:02 +0200

    tweaked

--- components/Tooltip.jsx
+++ components/Tooltip.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
 
 const Tooltip = ({ icon = 'fas fa-question-circle', children, className }) => {
-  let classes = ['h-8 w-8 text-green-600 flex justify-center items-center'];
+  const classes = ['h-8 w-8 text-green-600 flex justify-center items-center'];
   if (className) {
     classes.push(className);
   }

3f4b7de948e3360c89ab027b4b54e45442acda7a
victorocna <victorocnarescu@gmail.com>
2021-03-15 10:38:09 +0200

    update mutation

--- components/Todos/CheckTodo.jsx
+++ components/Todos/CheckTodo.jsx
@@ -1,20 +1,12 @@
-import { useMutation, useQueryClient } from 'react-query';
 import { Checkbox } from '../Forms';
 import { checkTodo } from '../../api/todo';
-import { toaster } from '../../functions';
+import { useMutation } from '../../hooks';
 
 const CheckTodo = ({ id, done }) => {
-  const queryClient = useQueryClient();
-  const mutation = useMutation(checkTodo, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('todos');
-      toaster.success('Todo status updated');
-    },
-    onError: () => {
-      toaster.error('Error! Cannot update todo status');
-    },
+  const mutation = useMutation('todos', checkTodo, {
+    success: 'Todo status updated',
+    error: 'Error! Cannot update todo status',
   });
-
   const handleClick = () => {
     mutation.mutate({ id, done });
   };

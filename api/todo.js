41a92c9382ad12bf09833d7e97c46a4099941ab3
victorocna <victorocnarescu@gmail.com>
2021-03-15 10:19:14 +0200

    update todo requests

--- api/todo.js
+++ api/todo.js
@@ -1,9 +1,5 @@
 import { axios } from '../services/api';
 
-export const readManyTodos = ({ pageParam = 0 }) => {
-  return axios.get(`todos?limit=3&offset=${pageParam}`);
-};
-
 export const createTodo = (data) => {
   return axios.post('todos', data);
 };

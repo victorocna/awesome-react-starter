ebfac5386f4a441089ef1056fce5e310c173599a
victorocna <victorocnarescu@gmail.com>
2021-03-15 16:08:26 +0200

    add menu button

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,4 +1,4 @@
-import { Menu, Tooltip } from '../components';
+import { Menu, Tooltip, MenuButton } from '../components';
 import { SayGoodbye, SayHello } from '../components/Demo';
 
 const Page = () => (
@@ -6,8 +6,11 @@ const Page = () => (
     <Menu />
     <main className="max w-full lg:col-span-5 p-4 lg:p-8 xl:px-12 space-y-4">
       <div className="flex items-center mb-12">
-        <h3 className="text-2xl font-semibold">Dashboard</h3>
-        <Tooltip placement="bottom">Just another tooltip</Tooltip>
+        <div className="flex flex-1">
+          <h3 className="text-2xl font-semibold">Dashboard</h3>
+          <Tooltip placement="bottom">Just another tooltip</Tooltip>
+        </div>
+        <MenuButton />
       </div>
       <div className="grid md:grid-cols-2 gap-4">
         <div className="bg-white rounded shadow p-4 space-y-4">

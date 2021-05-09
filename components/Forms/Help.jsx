effb3d46600471ca966ff9be8cf447935914792f
victorocna <victorocnarescu@gmail.com>
2021-05-09 17:51:00 +0300

    update help logic

--- components/Forms/Help.jsx
+++ components/Forms/Help.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { useField } from 'formik';
+import { useField, useFormikContext } from 'formik';
 
 const Help = ({ name, help }) => {
   // eslint-disable-next-line
   const [field, meta] = useField(name);
-  if (meta.touched && meta.error) {
-    return <div className="animated fadeIn text-red-700">{meta.error}</div>;
+  const { submitCount } = useFormikContext();
+
+  if (meta.touched && meta.error && submitCount) {
+    return <div className="animated fadeIn text-sm text-red-700 first-letter">{meta.error}</div>;
   }
   if (React.isValidElement(help)) {
     return help;
   }
   if (help) {
-    return <div className="text-gray-700">{help}</div>;
+    return <div className="text-sm text-gray-700">{help}</div>;
   }
   return false;
 };

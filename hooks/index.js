9aa78eaa2435e305885bbf41d3e4bc307d9b584e
victorocna <victorocnarescu@gmail.com>
2021-05-26 17:46:13 +0300

    update hooks

--- hooks/index.js
+++ hooks/index.js
@@ -1,8 +1,19 @@
 import useAxios from './use-axios';
+import useDebounce from './use-debounce';
 import useInfiniteQuery from './use-infinite-query';
 import useModal from './use-modal';
 import useMutation from './use-mutation';
+import useOnClickOutside from './use-onclick-outside';
 import useQuery from './use-query';
 import useSelect from './use-select';
 
-export { useAxios, useInfiniteQuery, useModal, useMutation, useQuery, useSelect };
+export {
+  useAxios,
+  useDebounce,
+  useInfiniteQuery,
+  useModal,
+  useMutation,
+  useOnClickOutside,
+  useQuery,
+  useSelect,
+};

dfc996cdffc6849b3eddd328ee5cc0d85288126a
victorocna <victorocnarescu@gmail.com>
2021-08-13 15:19:54 +0300

    fix menu hook

--- hooks/use-menu.js
+++ hooks/use-menu.js
@@ -2,14 +2,18 @@ import { useEffect, useState } from 'react';
 import { local } from 'store2';
 
 const useMenu = (key) => {
-  const initial = local.get(key);
-  const [open, setOpen] = useState(initial);
+  const [open, setOpen] = useState(false);
 
+  const initial = local.get(key);
   useEffect(() => {
     if (initial === null) {
+      // key does not exist in local storage
       setOpen(true);
+    } else {
+      // key exists in local storage
+      setOpen(initial);
     }
-  }, []);
+  }, [initial]);
 
   const toggleOpen = () => {
     setOpen((open) => {

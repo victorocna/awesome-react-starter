2844bf91867249a2a69552c9c16eec8a379c01ab
victorocna <victorocnarescu@gmail.com>
2021-08-16 12:19:11 +0300

    fix radio logic

--- components/Fields/Radio.jsx
+++ components/Fields/Radio.jsx
@@ -2,12 +2,16 @@ import { useContext } from 'react';
 import { RadioContext } from '.';
 
 const Radio = ({ children, ...props }) => {
-  const context = useContext(RadioContext);
-  props.checked = props.value === context.selectedValue;
+  const { name, selectedValue, onChange } = useContext(RadioContext);
+  props.defaultChecked = props.value === selectedValue;
+
+  // add context values to props
+  props.name = name;
+  props.onChange = onChange;
 
   return (
     <label className="inline-flex items-center relative cursor-pointer space-x-1 mr-2 max-w-full">
-      <input type="radio" role="radio" className="form-radio" {...props} {...context} />
+      <input type="radio" role="radio" className="form-radio" {...props} />
       <span className="min-w-14 inline-block text-center truncate">{children}</span>
     </label>
   );

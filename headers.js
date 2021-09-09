63bbbe66836c8337ad3b39e900ffd8fff6781b9f
victorocna <victorocnarescu@gmail.com>
2021-09-09 18:16:41 +0300

    update security headers using vercel documentation

--- headers.js
+++ headers.js
@@ -3,11 +3,36 @@
  * @see https://infosec.mozilla.org/guidelines/web_security
  */
 module.exports = [
-  { key: 'Strict-Transport-Security', value: 'max-age=31536000' },
-  { key: 'Server', value: 'Apache' },
-  { key: 'X-Content-Type-Options', value: 'nosniff' },
-  { key: 'X-Frame-Options', value: 'sameorigin' },
-  { key: 'X-XSS-Protection', value: '1' },
-  { key: 'Referrer-Policy', value: 'same-origin' },
-  { key: 'Permissions-Policy', value: 'geolocation=*' },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'Server',
+    value: 'Apache', // phony server value
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'sameorigin',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'geolocation=*', // allow specified policies here
+  },
 ];

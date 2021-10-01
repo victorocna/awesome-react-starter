19ccf85c7a4ad948ae77a7c15beafd2b6ed1bdf5
victorocna <victorocnarescu@gmail.com>
2021-10-01 12:35:37 +0300

    remove unused

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 import classnames from './classnames';
 import isDisabled from './is-disabled';
-import redirect from './redirect';
 import toaster from './toaster';
 
-export { classnames, isDisabled, redirect, toaster };
+export { classnames, isDisabled, toaster };

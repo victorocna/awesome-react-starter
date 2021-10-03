20e94df054754cd9a2a3ebe0ace2636d179b078d
victorocna <victorocnarescu@gmail.com>
2021-10-03 19:43:59 +0300

    update formik submit disabled logic

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 import ago from './ago';
 import coffee from './coffee';
-import isDisabled from './is-disabled';
 import toJSON from './to-json';
 
-export { ago, coffee, isDisabled, toJSON };
+export { ago, coffee, toJSON };

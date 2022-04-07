08c225257530f01a1019d809d08313d0207523ea
victorocna <victorocnarescu@gmail.com>
2022-04-07 16:56:30 +0300

    fix axios

--- examples/api/todo.js
+++ examples/api/todo.js
@@ -1,15 +1,15 @@
-import { axios } from '../../lib';
+import { axiosAuth } from '../../lib';
 
 export const createTodo = (data) => {
-  return axios.post('todos', data);
+  return axiosAuth.post('todos', data);
 };
 
 export const deleteTodo = (id) => {
-  return axios.delete(`todos/${id}`);
+  return axiosAuth.delete(`todos/${id}`);
 };
 
 export const checkTodo = ({ id, done }) => {
-  return axios({
+  return axiosAuth({
     url: `todos/${id}/check`,
     method: done ? 'delete' : 'post',
   });

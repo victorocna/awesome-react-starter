d90396f4fcf67fe3021b7ebe78857d95206ddf96
victorocna <victorocnarescu@gmail.com>
2022-04-07 16:57:17 +0300

    fix loading

--- components/Formik/Submit.jsx
+++ components/Formik/Submit.jsx
@@ -2,15 +2,15 @@ import { useFormikContext } from 'formik';
 import { Button } from '..';
 
 const Submit = ({ children, isLoading, ...props }) => {
-  const { isSubmitting, isValid, validateOnMount } = useFormikContext();
-  const disabled = isSubmitting || (validateOnMount && !isValid);
+  const { isValid, validateOnMount } = useFormikContext();
+  const disabled = isLoading || (validateOnMount && !isValid);
 
   return (
     <div className="flex items-center">
       <Button type="submit" className="button full primary" disabled={disabled} {...props}>
         {children}
       </Button>
-      {isSubmitting && <img src="/icons/loading.gif" alt="loading" className="mx-1 w-6" />}
+      {isLoading && <img src="/icons/loading.gif" alt="loading" className="mx-1 w-6" />}
     </div>
   );
 };

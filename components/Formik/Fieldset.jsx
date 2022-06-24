184c1a49a0570c2a7eeb0561381e882e0dab019b
Sebastian Pavel <pavel.sebastian.ionut@gmail.com>
2022-06-24 19:13:46 +0300

    Update Fieldset.jsx
    Update error message.
    This change will work on nested objects also

--- components/Formik/Fieldset.jsx
+++ components/Formik/Fieldset.jsx
@@ -1,20 +1,18 @@
 import { useFormikContext } from 'formik';
 import { classnames } from '../../lib';
+import { get } from 'lodash';
 
 const Fieldset = ({ label, help, name, children }) => {
   const { submitCount, touched, errors } = useFormikContext();
-  const hasError = touched[name] && errors[name] && submitCount > 0;
-
+  const hasError = get(touched, name) && get(errors, name) && submitCount > 0;
   return (
     <fieldset className={classnames(hasError && 'has-error')}>
-      {label && (
-        <label htmlFor={name} className="form-label w-full cursor-pointer mb-0">
-          {label}
-        </label>
-      )}
+      <label htmlFor={name} className="form-label w-full cursor-pointer mb-0">
+        {label}
+      </label>
       {children}
       <div className="form-help text-sm text-secondary first-letter">
-        {hasError ? errors[name] : help}
+        {hasError ? get(errors, name) : help}
       </div>
     </fieldset>
   );

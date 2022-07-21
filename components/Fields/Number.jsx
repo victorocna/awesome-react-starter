89213fcdb612df119c15ed24caa387911172b125
victorocna <victorocnarescu@gmail.com>
2022-07-21 15:30:50 +0300

    update number input

--- components/Fields/Number.jsx
+++ components/Fields/Number.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Number = (props) => {
-  return <input type="number" inputMode="number" className="form-input" {...props} />;
+  const handleWheel = (event) => {
+    // prevents changing value of input[type=number]:focus, when user scrolls down the page
+    event.target.blur();
+  };
+
+  return (
+    <input
+      type="number"
+      inputMode="number"
+      className="form-input"
+      onWheel={handleWheel}
+      {...props}
+    />
+  );
 };
 
 export default Number;

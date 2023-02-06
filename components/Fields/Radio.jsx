f7ed0f85d9361833d750268f4e7453fd534c0301
Eusebiu Amarii <amariieusebiu18@stud.ase.ro>
2023-02-06 14:13:55 +0200

    fix typo

--- components/Fields/Radio.jsx
+++ components/Fields/Radio.jsx
@@ -10,7 +10,7 @@ const Radio = ({ children, ...props }) => {
   props.onChange = onChange;
 
   return (
-      <div class="mt-2 ">
+      <div className="mt-2 ">
         <label className="inline-flex items-center ">
           <input  type="radio" role="radio" className="radio" {...props} />
           <span className="ml-2">{children}</span>

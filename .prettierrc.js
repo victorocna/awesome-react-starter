2964df124803d04b7e4e446e2a6688c9cd666fe2
victorocna <victorocnarescu@gmail.com>
2023-07-13 12:30:17 +0300

    update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,12 @@
 module.exports = {
+  // Prettier default config
   trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   arrowParens: 'always',
+
+  // Prettier sort import config
   importTypeOrder: ['NPMPackages', 'localImports'],
   sortingMethod: 'alphabetical',
 };

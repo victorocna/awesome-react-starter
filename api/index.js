0d338305c1e952677316deb9f1efc56eb3702b7a
victorocna <victorocnarescu@gmail.com>
2023-07-16 14:02:11 +0300

    remove index files

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import confirm from './confirm';
-import forgot from './forgot';
-import login from './login';
-import logout from './logout';
-import refreshToken from './refresh-token';
-import reset from './reset';
-import signup from './signup';
-
-export { confirm, forgot, login, logout, refreshToken, reset, signup };

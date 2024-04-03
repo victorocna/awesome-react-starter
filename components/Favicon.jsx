cf132fcc0a7a4b75a196cf3893ed8d984a194ce4
victorocna <victorocnarescu@gmail.com>
2024-04-03 10:54:36 +0300

    update head components

--- components/Favicon.jsx
+++ components/Favicon.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Favicon = () => (
-  <>
-    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
-    <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-  </>
-);
+const Favicon = () => {
+  // No need for importing Head from next/head
+
+  return (
+    <>
+      <link rel="apple-touch-icon" href="/images/favicon.png" />
+      <link rel="shortcut icon" sizes="any" href="/images/favicon.ico" type="image/png" />
+      <link rel="icon" sizes="any" href="/images/favicon.ico" type="image/png" />
+    </>
+  );
+}
 
 export default Favicon;
--- components/OpenGraph.jsx
+++ components/OpenGraph.jsx
@@ -1,18 +1,22 @@
 import { baseurl, description, image, sitename, title } from '@site.config';
+import Head from 'next/head';
 
 const OpenGraph = () => {
-  return (
-    <>
-      <meta property="og:site_name" content={sitename} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="og:url" content={baseurl} />
-      <meta property="og:type" content="website" />
+  // The key property is used to prevent duplicate meta tags
+  // https://nextjs.org/docs/api-reference/next/head#multiple-instances
 
-      <meta itemProp="name" content={title} />
-      <meta itemProp="description" content={description} />
-    </>
+  return (
+    <Head>
+      <meta property="og:site_name" key="og:site_name" content={sitename} />
+      <meta property="og:title" key="og:title" content={title} />
+      <meta property="og:description" key="og:description" content={description} />
+      <meta property="og:image" key="og:image" content={image} />
+      <meta property="og:url" key="og:url" content={baseurl} />
+      <meta property="og:type" key="og:type" content="website" />
+
+      <meta itemProp="name" key="itepProp:name" content={title} />
+      <meta itemProp="description" key="itemProp:description" content={description} />
+    </Head>
   );
 };
 

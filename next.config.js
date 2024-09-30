bc6284dcb4667904bc58c8119b16f5bbd6b7d56e
Eusebiu Amarii <amariieusebiu18@stud.ase.ro>
2024-09-30 12:17:56 +0300

    add font-awesome key

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
     APP_BASE_URL: process.env.APP_BASE_URL,
     JWT_TOKEN_NAME: process.env.JWT_TOKEN_NAME,
     RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
+    FONT_AWESOME_KEY: process.env.FONT_AWESOME_KEY,
     SHOW_FORMIK_DEBUG: process.env.SHOW_FORMIK_DEBUG,
     SHOW_LOGS_EVERYWHERE: process.env.SHOW_LOGS_EVERYWHERE,
   },

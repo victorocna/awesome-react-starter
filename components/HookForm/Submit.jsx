c328d1743149eb3b3fa8c258899b8fb31e9ac04e
victorocna <victorocnarescu@gmail.com>
2024-10-31 18:20:27 +0200

    Fix button styles

--- components/HookForm/Submit.jsx
+++ components/HookForm/Submit.jsx
@@ -10,7 +10,7 @@ const Submit = ({ children, isLoading, ...props }) => {
   props.disabled = disabled;
 
   return (
-    <div className="flex items-center relative">
+    <div className="inline-flex items-center relative">
       <Button type="submit" className="button full primary" {...props}>
         <div>{children}</div>
       </Button>

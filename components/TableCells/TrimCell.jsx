399e900af3665acf9599f68a99e5266ad410f0eb
Bercaru Robert <robert.bercaru@chesscoders.com>
2025-01-21 10:57:39 +0200

    modify TrimCell component

--- components/TableCells/TrimCell.jsx
+++ components/TableCells/TrimCell.jsx
@@ -1,31 +1,11 @@
-import { classnames } from '@lib';
+import { Trim } from "@components";
 
 const TrimCell = ({ column, row: { original } }) => {
   if (!original) {
     return '';
   }
-  const { extraClass } = column;
 
-  const format = (value, limit = 50) => {
-    if (value.length < limit) {
-      return value;
-    }
-    return (
-      <>
-        <span>{value.substring(0, limit)}</span>
-        <span>...</span>
-      </>
-    );
-  };
-
-  return (
-    <div
-      className={classnames('cursor-default overflow-x-auto whitespace-nowrap', extraClass)}
-      title={original}
-    >
-      {format(original)}
-    </div>
-  );
+  return <Trim column={column} value={original} />;
 };
 
 export default TrimCell;

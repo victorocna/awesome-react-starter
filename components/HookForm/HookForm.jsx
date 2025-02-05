d50007dfdbc3dd8d22ea99207b15583d4011ad76
victorocna <victorocnarescu@gmail.com>
2025-02-05 18:38:15 +0200

    Add handle fail

--- components/HookForm/HookForm.jsx
+++ components/HookForm/HookForm.jsx
@@ -2,15 +2,29 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { FormProvider, useForm } from 'react-hook-form';
 
 const HookForm = ({ children, initialValues, validationSchema, onSubmit }) => {
-  // Create a new form context
-  const { handleSubmit, ...methods } = useForm({
+  const methods = useForm({
     resolver: yupResolver(validationSchema),
     defaultValues: initialValues,
   });
 
+  // Function to mark all fields as touched
+  const markAllFieldsTouched = () => {
+    const values = methods.getValues(); // Get all registered fields
+    Object.keys(values).forEach((field) => {
+      methods.setValue(field, values[field], { shouldTouch: true });
+    });
+  };
+
+  const handleSubmit = async (data) => {
+    await onSubmit(data, methods);
+  };
+  const handleFail = async () => {
+    markAllFieldsTouched();
+  };
+
   return (
     <FormProvider {...methods}>
-      <form onSubmit={handleSubmit((data) => onSubmit(data, methods))}>{children}</form>
+      <form onSubmit={methods.handleSubmit(handleSubmit, handleFail)}>{children}</form>
     </FormProvider>
   );
 };

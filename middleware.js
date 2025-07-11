51cd012904b3fdaaa3e72a66c24f7dff8df6f255
victorocna <victorocnarescu@gmail.com>
2025-07-11 15:30:00 +0300

    Format code and fix imports

--- middleware.js
+++ middleware.js
@@ -14,19 +14,22 @@ const PUBLIC_FILES_OR_DIRECTORIES = [
   'manifest.json',
 ];
 
-const isPublicFileOrDirectory = (pathname) =>
-  PUBLIC_FILES_OR_DIRECTORIES.some((item) => pathname.startsWith(`/${item}`));
+const isPublicFileOrDirectory = (pathname) => {
+  return PUBLIC_FILES_OR_DIRECTORIES.some((item) => pathname.startsWith(`/${item}`));
+};
 
 export default async function middleware(req) {
   const url = req.nextUrl.clone();
   // Check if maintenance mode is enabled via environment variable
   const isMaintenanceMode = process.env.MAINTENANCE_MODE === 'yes';
 
+  // If maintenance mode is not enabled, redirect from /maintenance to the homepage
   if (!isMaintenanceMode && url.pathname === '/maintenance') {
     url.pathname = '/';
     return NextResponse.redirect(url);
   }
 
+  // If maintenance mode is enabled, redirect all requests to /maintenance
   if (
     isMaintenanceMode &&
     url.pathname !== '/maintenance' &&
--- pages/maintenance.js
+++ pages/maintenance.js
@@ -1,11 +1,11 @@
-import { site_name } from '@site.config';
+import { sitename } from '@site.config';
 
 const Page = () => {
   return (
     <main className="cover flex min-h-screen flex-col items-center justify-center px-4 py-8">
       <div className="my-8 flex w-full max-w-xl flex-col rounded-lg bg-white px-4 py-8 lg:px-12">
         <div className="text-center">
-          <img className="mb-6 mx-auto w-32 md:w-44" src="/images/logo.png" alt={site_name} />
+          <img className="mb-6 mx-auto w-32 md:w-44" src="/images/logo.png" alt={sitename} />
           <p className="mb-6 font-body text-lg md:text-xl">
             We are currently performing maintenance to improve user experience.
           </p>
